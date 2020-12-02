20fa88c7d69aa81639390e1fb3853791084cc6e8
Mr.Lin <278971585@qq.com>
2020-12-02 18:42:15 +0800

    菜单调用管理系统

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/BegLord.java
@@ -0,0 +1,31 @@
+package com.ruoyi.common;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.http.HttpUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 管理系统相关调用
+ *
+ * @author Mr.Lin
+ */
+@Component
+public class BegLord {
+
+    /**
+     * 设置请求的统一前缀
+     */
+    @Value("${lord.url}")
+    private String url;
+
+    public AjaxResult getRouters() {
+        String res = HttpUtils.sendGet(url + "/open/getRouters", "projectId=1");
+        AjaxResult ajaxResult = JSON.parseObject(res, new TypeReference<AjaxResult>() {
+        });
+        return ajaxResult;
+    }
+
+}
