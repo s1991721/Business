0f654a753ad7e4b22bc2c19d8ab183aea36cb625
Mr.Lin <278971585@qq.com>
2020-12-03 11:08:44 +0800

    调用管理系统登陆相关接口

--- ruoyi-common/src/main/java/com/ruoyi/common/BegLord.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/BegLord.java
@@ -3,7 +3,9 @@ package com.ruoyi.common;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.domain.entity.SysUser;
 import com.ruoyi.common.utils.http.HttpUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -28,4 +30,10 @@ public class BegLord {
         return ajaxResult;
     }
 
+    public SysUser getUserByName(String name) {
+        String json = HttpUtils.sendPost(url + "/open/getUserByName", "username=" + name);
+        SysUser user = new SysUser();
+        BeanUtils.copyProperties(JSON.parseObject(json, SysUser.class), user);
+        return user;
+    }
 }
